4ab90c936316a255b4ae161340a7d21edd9708ba
Jalen-Huang <Jalen.huang55@gmail.com>
2024-07-29 15:38:35 -0700

    Added multi-page functionality to settings sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import SideBarButton from "./SideBarButton";
 import "./SideBar.css";
 
 const SideBar = () => {
   const handleClick = () => {
     console.log("Button was clicked");
-  };
+  }
+  const Settings1 = () => {
+    setPage("page1");
+  }
+  const Settings2 = () => {
+    setPage("page2");
+  }
+  const Back = () => {
+    setPage("main");
+  }
+  const [page, setPage] = useState("main");
   return (
     <div class="SideBar">
-      <SideBarButton onClick={handleClick}>Button 1</SideBarButton>
-      <SideBarButton>Button 2</SideBarButton>
-      <SideBarButton>Button 3</SideBarButton>
-      <SideBarButton>Button 4</SideBarButton>
+      {page=="main" && <SideBarButton onClick={Settings1}>Settings1</SideBarButton>}
+      {page=="main" && <SideBarButton onClick={Settings2}>Settings2</SideBarButton>}
+      {page=="page1" && <SideBarButton>Button 3</SideBarButton>}
+      {page=="page2" && <SideBarButton>Button 3</SideBarButton>}
+      {!(page=="main") && <SideBarButton onClick={Back}>Back</SideBarButton>}
     </div>
   );
 };
