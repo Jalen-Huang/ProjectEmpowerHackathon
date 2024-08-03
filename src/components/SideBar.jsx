869a6bc522df27ec12391aec428dcb5c918ae84d
Jalen-Huang <Jalen.huang55@gmail.com>
2024-08-02 19:29:58 -0700

    Added saving functionality to different fonts and color themes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -23,16 +23,19 @@ const SideBar = () => {
   const Verdana = () => {
     var root = document.querySelector(':root');
     root.style.setProperty("--font-family", "Verdana");
+    localStorage.setItem("font-family", JSON.stringify("Verdana"))
   }
 
   const FranklinGothic = () => {
     var root = document.querySelector(':root');
     root.style.setProperty("--font-family", "Franklin Gothic Medium");
+    localStorage.setItem("font-family", JSON.stringify("Franklin Gothic Medium"))
   }
 
   const TimesNewRoman = () => {
     var root = document.querySelector(':root');
     root.style.setProperty("--font-family", "Times New Roman");
+    localStorage.setItem("font-family", JSON.stringify("Times New Roman"))
   }
 
   const Settings2 = () => {
@@ -46,6 +49,12 @@ const SideBar = () => {
     root.style.setProperty("--tertiary", "#DEF2F1");
     root.style.setProperty("--quarternary", "#17252A");
     root.style.setProperty("--quinary", "#FEFFFF");
+
+    localStorage.setItem("primary", JSON.stringify("#2B7A78"))
+    localStorage.setItem("secondary", JSON.stringify("#3AAFA9"))
+    localStorage.setItem("tertiary", JSON.stringify("#DEF2F1"))
+    localStorage.setItem("quarternary", JSON.stringify("#17252A"))
+    localStorage.setItem("quinary", JSON.stringify("#FEFFFF"))
   }
 
   const WarmColors = () => {
@@ -55,6 +64,12 @@ const SideBar = () => {
     root.style.setProperty("--tertiary", "#F0EBF4");
     root.style.setProperty("--quarternary", "#A1C3D1");
     root.style.setProperty("--quinary", "#B39BC8");
+
+    localStorage.setItem("primary", JSON.stringify("#E64398"))
+    localStorage.setItem("secondary", JSON.stringify("#F172A1"))
+    localStorage.setItem("tertiary", JSON.stringify("#F0EBF4"))
+    localStorage.setItem("quarternary", JSON.stringify("#A1C3D1"))
+    localStorage.setItem("quinary", JSON.stringify("#B39BC8"))
   }
 
   const HalfHourToggle = () => {
@@ -73,6 +88,37 @@ const SideBar = () => {
     const localPage = JSON.parse(localStorage.getItem("page"));
     if (localPage) {
       setPage(localPage);
+    }
+    const font = JSON.parse(localStorage.getItem("font-family"));
+    if (font) {
+      var root = document.querySelector(':root');
+      root.style.setProperty("--font-family", font);
+    }
+    const primary = JSON.parse(localStorage.getItem("primary"));
+    const secondary = JSON.parse(localStorage.getItem("secondary"));
+    const tertiary = JSON.parse(localStorage.getItem("tertiary"));
+    const quarternary = JSON.parse(localStorage.getItem("quarternary"));
+    const quinary = JSON.parse(localStorage.getItem("quinary"));
+    
+    if (primary) {
+      var root = document.querySelector(':root');
+      root.style.setProperty("--primary", primary);
+    }
+    if (secondary) {
+      var root = document.querySelector(':root');
+      root.style.setProperty("--secondary", secondary);
+    }
+    if (tertiary) {
+      var root = document.querySelector(':root');
+      root.style.setProperty("--tertiary", tertiary);
+    }
+    if (quarternary) {
+      var root = document.querySelector(':root');
+      root.style.setProperty("--quarternary", quarternary);
+    }
+    if (quinary) {
+      var root = document.querySelector(':root');
+      root.style.setProperty("--quinary", quinary);
     }
   }, []);
 
